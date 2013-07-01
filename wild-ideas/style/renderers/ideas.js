aa2f142714a46d67409c36682cedde5f77de84c1
Geraint Luff <luffgd@gmail.com>
2013-07-01 07:39:54 +0100

    Make links generic for Wild Ideas table

--- wild-ideas/style/renderers/ideas.js
+++ wild-ideas/style/renderers/ideas.js
@@ -4,20 +4,40 @@ Jsonary.render.register(Jsonary.plugins.Generator({
 			columns: [
 			],
 			titles: {
-				"link-edit": "",
-				"link-delete": ""
 			},
 			rowRenderHtml: function (data, context) {
 				var result = '';
-				if (context.uiState.editData) {
-					result += this.defaultRowRenderHtml(context.uiState.editData, context);
+				if (context.uiState.expand) {
+					result += this.defaultRowRenderHtml(data, context);
+					result += '<td class="json-array-table-full" colspan="' + this.columns.length + '">';
+					if (context.uiState.expand === true) {
+						result += context.renderHtml(data);
+					} else {
+						result += context.renderHtml(context.uiState.expand);
+					}
+					result += '</td>';
+				} else if (context.uiState.linkRel) {
+					var link = data.links(context.uiState.linkRel)[context.uiState.linkIndex || 0];
+					if (context.uiState.linkData) {
+						if (link.rel == "edit") {
+							result += this.defaultRowRenderHtml(context.uiState.linkData, context);
+						} else {
+							result += this.defaultRowRenderHtml(data, context);
+							result += '<td class="json-array-table-full" colspan="' + this.columns.length + '">';
+							result += context.actionHtml('<span class="button action">confirm</span>', 'link-confirm', context.uiState.linkRel, context.uiState.linkIndex);
+							result += context.actionHtml('<span class="button action">cancel</span>', 'link-cancel');
+							result += context.renderHtml(context.uiState.linkData);
+							result += '</td>';
+						}
+					} else {
+						result += this.defaultRowRenderHtml(data, context);
+						result += '<td class="json-array-table-full" colspan="' + this.columns.length + '">';
+						result += context.actionHtml('<span class="button action">confirm</span>', 'link-confirm', context.uiState.linkRel, context.uiState.linkIndex);
+						result += context.actionHtml('<span class="button action">cancel</span>', 'link-cancel');
+						result += '</td>';
+					}
 				} else {
 					result += this.defaultRowRenderHtml(data, context);
-				}
-				if (context.uiState.expand) {
-					result += '<td class="json-array-table-full" colspan="' + this.columns.length + '">';
-					result += context.renderHtml(data);
-					result += '</td>';
 				}
 				return result;
 			},
@@ -56,7 +76,7 @@ Jsonary.render.register(Jsonary.plugins.Generator({
 					return result + '</td>';
 				}
 			},
-			rowAction: function (data, context, actionName) {
+			rowAction: function (data, context, actionName, arg1, arg2) {
 				if (actionName == "expand") {
 					if (context.uiState.expand) {
 						delete context.uiState.expand;
@@ -71,16 +91,55 @@ Jsonary.render.register(Jsonary.plugins.Generator({
 						editLink.follow(context.uiState.editData, false);
 					}
 					delete context.uiState.editData;
-				} else if (actionName == "delete") {
-					context.uiState.deleteConfirm = true;
-				} else if (actionName == "delete-confirm") {
-					var deleteLink = data.getLink("delete");
-					if (deleteLink) {
-						deleteLink.follow(undefined, false);
+				} else if (actionName == "link") {
+
+					var linkRel = arg1, linkIndex = arg2
+					var link = data.links(linkRel)[linkIndex || 0];
+					if (link.submissionSchemas.length) {
+						context.uiState.linkRel = linkRel;
+						context.uiState.linkIndex = linkIndex;
+						var linkData = Jsonary.create();
+						linkData.addSchema(link.submissionSchemas);
+						context.uiState.linkData = linkData;
+						link.submissionSchemas.createValue(function (value) {
+							linkData.setValue(value);
+						});
+						delete context.uiState.expand;
+					} else if (link.rel == "edit") {
+						context.uiState.linkRel = linkRel;
+						context.uiState.linkIndex = linkIndex;
+						context.uiState.linkData = data.editableCopy();
+						delete context.uiState.expand;
+					} else if (link.method != "GET") {
+						context.uiState.linkRel = linkRel;
+						context.uiState.linkIndex = linkIndex;
+						delete context.uiState.linkData;
+						delete context.uiState.expand;
+					} else {
+						var targetExpand = (link.rel == "self") ? true : link.href;
+						if (context.uiState.expand == targetExpand) {
+							delete context.uiState.expand;
+						} else {
+							context.uiState.expand = targetExpand;
+						}
 					}
-					delete context.uiState.deleteConfirm;
-				} else if (actionName == "delete-cancel") {
-					delete context.uiState.deleteConfirm;
+				} else if (actionName == "link-confirm") {
+					var linkRel = arg1, linkIndex = arg2
+					context.uiState.linkRel = linkRel;
+					context.uiState.linkIndex = linkIndex;
+					var link = data.links(linkRel)[linkIndex || 0];
+					if (link) {
+						link.follow(context.uiState.linkData, false);
+					}
+					delete context.uiState.linkRel;
+					delete context.uiState.linkIndex;
+					delete context.uiState.linkData;
+					delete context.uiState.expand;
+				} else if (actionName == "link-cancel") {
+					delete context.uiState.linkRel;
+					delete context.uiState.linkIndex;
+					delete context.uiState.linkData;
+					delete context.uiState.expand;
 				}
 				return true;
 			}
@@ -114,23 +173,41 @@ Jsonary.render.register(Jsonary.plugins.Generator({
 				}
 			}
 		}
-		function addColumnsFromLink(link) {
+		function addColumnsFromLink(linkDefinition, index) {
+			var columnName = "link$" + index + "$" + linkDefinition.rel;
+			tableConfig.titles[columnName] = linkDefinition.title || linkDefinition.rel();
+			tableConfig.columns.push(columnName);
 			
+			var confirmAction = "link-confirm", confirmTitle = null;
+			if (linkDefinition.rel() == "edit") {
+				confirmTitle = "save";
+			}
+			
+			tableConfig.cellRenderHtml[columnName] = function (data, context) {
+				var result = '<td>';
+				if (!context.parent.uiState.linkRel) {
+					var links = data.links(linkDefinition.rel());
+					for (var i = 0; i < links.length; i++) {
+						var link = links[i];
+						if (link.definition == linkDefinition) {
+							result += context.parent.actionHtml(link.title || link.rel, 'link', link.rel, i || undefined);
+						}
+					}
+				} else if (confirmTitle) {
+					var link = data.links(context.parent.uiState.linkRel)[context.parent.uiState.linkIndex || 0];
+					if (link.definition == linkDefinition) {
+						result += context.parent.actionHtml(confirmTitle, confirmAction, link.rel, context.parent.uiState.linkIndex);
+					}
+				}
+				return result + '</td>';
+			};
 		}
 		var itemSchemas = data.schemas().indexSchemas(0).getFull();
 		if (data.readOnly()) {
-			if (itemSchemas.links('delete').length > 0) {
-				tableConfig.columns.push('link-delete');
-			}
-			if (itemSchemas.links('edit').length > 0) {
-				tableConfig.columns.push('link-edit');
-			}
 			var links = itemSchemas.links();
 			for (var i = 0; i < links.length; i++) {
 				var link = links[i];
-				if (link.rel != "delete" && link.rel != "edit") {
-					addColumnsFromLink(link);
-				}
+				addColumnsFromLink(link, i);
 			}
 		}
 		addColumnsFromSchemas(itemSchemas);
